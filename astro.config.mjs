8c125bfb10feb64a396554b6f176d1455b6f26a8
H-Pascual <hector.pascual.marin@gmail.com>
2025-07-22 16:30:51 +0200

    Hecho traducciones, creando componentes para web en inglés

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static'; // ⬅️ Usa el adaptador estático de Vercel
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static', // ⬅️ Requerido para sitios estáticos
