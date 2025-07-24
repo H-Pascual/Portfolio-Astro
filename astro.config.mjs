cdeab6ce59607f2387f63eba28daa38236b4f39f
H-Pascual <hector.pascual.marin@gmail.com>
2025-07-24 15:50:49 +0200

    Creado robots y sitemap y optimizados los images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static', // ⬅️ Requerido para sitios estáticos
   adapter: vercel({}),
+  site: 'https://www.hectorpascual.com/',
+  integrations: [
+    sitemap(),
+  ],
 });
